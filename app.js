4ce55e76a01b1b4f0d4912c9e9caad6b01c556b5
angelcuevas <angel.p.cuevas@gmail.com>
2024-05-08 00:27:33 -0300

    added: dockerfile

--- app.js
+++ app.js
@@ -1,56 +1,33 @@
-const cluster = require('cluster')
-const os = require('os'); 
 const express = require('express');
 
-const cpuQuantity = os.cpus().length; 
+const port = 8080; 
+const app = express();
 
-
-
-if(cluster.isPrimary){
-    console.log(`${process.pid} - Is primary? ${cluster.isPrimary}, starting a worker process`)
-    for (let i = 0; i < cpuQuantity; i++) {
-        cluster.fork()
+app.get('/operation/simple',(req, res)=>{
+    let sum = 0; 
+    for (let i = 0; i < 1_000_000; i++) {
+        sum+=i; 
     }
 
-    // cluster.on('message', (worker, message)=>{
-    //     console.log('---->', worker.process.pid, message)
-    // })
-    cluster.on('exit', (worker)=>{
-        console.log(`I've been killed, ${worker.process.pid} `)
-        cluster.fork()
+    console.log(`${process.pid} - sum: ${sum}`)
+
+    res.send({
+        status:'success',
+        payload: sum
     })
-}else{
-    console.log(`${process.pid} Is primary? ${cluster.isPrimary}, I'm a worker process`)
-    //process.send({message:'I just started'})
-    const port = 8080; 
-    const app = express();
+})
 
-    app.get('/operation/simple',(req, res)=>{
-        let sum = 0; 
-        for (let i = 0; i < 1_000_000; i++) {
-            sum+=i; 
-        }
+app.get('/operation/complex',(req, res)=>{
+    let sum = 0; 
+    for (let i = 0; i < 500_000_000; i++) {
+        sum+=i; 
+    }
+    console.log(`${process.pid} - sum: ${sum}`)
 
-        console.log(`${process.pid} - sum: ${sum}`)
-
-        res.send({
-            status:'success',
-            payload: sum
-        })
+    res.send({
+        status:'success',
+        payload: sum
     })
+})    
 
-    app.get('/operation/complex',(req, res)=>{
-        let sum = 0; 
-        for (let i = 0; i < 500_000_000; i++) {
-            sum+=i; 
-        }
-        console.log(`${process.pid} - sum: ${sum}`)
-
-        res.send({
-            status:'success',
-            payload: sum
-        })
-    })    
-
-    app.listen(port, ()=>console.log(`up and running on port ${port} pid ${process.pid}`))
-}
+app.listen(port, ()=>console.log(`up and running on port ${port} pid ${process.pid}`))
